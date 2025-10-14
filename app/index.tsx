ffb573988092ad7655147fc3dafa903a1c350e7c
A-4-Atom <vcikas2626@gmail.com>
2025-10-14 20:56:52 +0530

    removed unused code

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View className='flex items-center justify-center h-full'>
+      <Text className='text-xl font-bold'>Index</Text>
+    </View>
+  )
+}
+
+export default Index
